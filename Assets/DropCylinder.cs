46349f056f490f1adf2272186016f71406bad487
Vicentea47540 <104469598+Vicentea47540@users.noreply.github.com>
2022-12-08 23:15:55 +0000

    Flocking part 1

--- Assets/DropCylinder.cs
+++ Assets/DropCylinder.cs
@@ -18,19 +18,8 @@ public class DropCylinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hitInfo;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
-            {
-                Instantiate(obstacle, hitInfo.point, obstacle.transform.rotation);
-                foreach (GameObject a in agents)
-                {
-                    a.GetComponent<AIControl>().DetectNewObstacle(hitInfo.point);
-                }
-               
-            }
-        }
+
+            
+        
     }
 }
