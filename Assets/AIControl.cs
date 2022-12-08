4f1ed2e6586693bc2f2ec67025fc3abab91f323c
Vicentea47540 <104469598+Vicentea47540@users.noreply.github.com>
2022-12-08 22:48:36 +0000

    Fleeing

--- Assets/AIControl.cs
+++ Assets/AIControl.cs
@@ -1,10 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AIControl : MonoBehaviour {
 
     GameObject[] goalLocations;
     UnityEngine.AI.NavMeshAgent agent;
     Animator anim;
+
+    float speedMult;
+
+    float detectionRadius = 5.0f;
+    float fleeRadius = 10.0f;
+
+    void ResetAgent(){
+        speedMult = Random.Range(0.1f, 1.5f);
+        agent.speed = 2 * speedMult;
+        agent.angularSpeed = 120;
+        anim.SetFloat("speedMult", speedMult);
+        anim.SetTrigger("isWalking");
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position) {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius) {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newgoal, path);
+
+            if (path.status == NavMeshPathStatus.PathInvalid) {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 360;
+            }
+
+        }
+    }
 
 
     // Use this for initialization
@@ -13,9 +48,9 @@ public class AIControl : MonoBehaviour {
         agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
         anim = this.GetComponent<Animator>();
-        anim.SetFloat("wOffset", Random.Range(0, 1));
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
         anim.SetTrigger("isWalking");
-        float sm = Random.Range(0.5f, 1.5f);
+        float sm = Random.Range(0.1f, 1.5f);
         anim.SetFloat("speedMult", sm);
         agent.speed *= sm;
     }
