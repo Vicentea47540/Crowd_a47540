832694877fc09b25d2d8e484436145cfd30f2eb6
Vicentea47540 <104469598+Vicentea47540@users.noreply.github.com>
2022-12-08 23:18:49 +0000

    Flock fix

--- Assets/FlockManager.cs
+++ Assets/FlockManager.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FlockManager : MonoBehaviour
-{
+public class FlockManager : MonoBehaviour {
+
     public GameObject fishPrefab;
     public int numFish = 20;
     public GameObject[] allFish;
-    public Vector3 swimLimits = new Vector3(5, 5, 5);
+    public Vector3 swimLimits = new Vector3(5.0f, 5.0f, 5.0f);
 
     [Header("Fish Settings")]
     [Range(0.0f, 5.0f)]
@@ -18,24 +16,22 @@ public class FlockManager : MonoBehaviour
     public float neighbourDistance;
     [Range(0.0f, 5.0f)]
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Start() {
+
         allFish = new GameObject[numFish];
-        for (int i = 0; i < numFish; i++)
-        {
+        for (int i = 0; i < numFish; ++i) {
+
             Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
-                Random.Range(-swimLimits.y, swimLimits.y),
-                Random.Range(-swimLimits.z, swimLimits.z));
+                                                                Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.x, swimLimits.x));
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             allFish[i].GetComponent<Flock>().myManager = this;
         }
-        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
